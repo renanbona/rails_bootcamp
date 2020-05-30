73bdaa392414e77f7e036ac055d6c9916d436e9c
Renan Bona <rcbp3094@gmail.com>
2020-05-30 13:38:32 -0300

    Create end points index, new, create, edit and update for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,43 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    if @portfolio.save
+      redirect_to portfolios_path, flash[:notice] = { message: 'Portfolio Created' }
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+
+    if @portfolio.update(portfolio_params)
+      redirect_to portfolios_path, flash[:notice] = { message: 'Portfolio updated' }
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params
+      .require(:portfolio)
+      .permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 end
