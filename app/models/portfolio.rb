ac38c90ac445f2e356e71acb4fb7d8c13eef804b
Renan Bona <rcbp3094@gmail.com>
2020-07-06 18:31:19 -0300

    Set defaults values to portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 class Portfolio < ApplicationRecord
-  validates :title, :subtitle, :body, presence: true
+  after_initialize :set_defaults
+
+  validates :title, :body, presence: true
+
+  private
+
+  def set_defaults
+    self.main_image ||= 'http://placehold.it/600x400'
+    self.thumb_image ||= 'http://placehold.it/350x200'
+  end
 end
