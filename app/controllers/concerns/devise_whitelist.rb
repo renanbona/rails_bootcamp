14393d371683bc51c19a3d36e3ddd42dac331c17
Renan Bona <rcbp3094@gmail.com>
2020-07-11 11:11:36 -0300

    Create DeviseWhitelis concern and move the devise permitted parameters from ApplicationController

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end
